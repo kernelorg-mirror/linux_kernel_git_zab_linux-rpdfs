7bd4f8ddc08daa8df303b0090c1bdbc842b25795
Zach Brown <zab@zabbo.net>
2026-02-25 11:18:40 -0800

    rpdfs: Add block alloc core
    The balloc core manges regions of free blocks.  It's no more than a few
    functions for working with the regions and per-cpu and global lists to
    track them.  The block cache assembles the regions from messages from
    the network and hands them off.  The transaction engine then consumes
    them.
    
    Signed-off-by: Zach Brown <zab@zabbo.net>

--- fs/rpdfs/balloc.c
+++ fs/rpdfs/balloc.c
@@ -1,121 +1,262 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 
-#include <linux/atomic.h>
+#include <linux/kernel.h>
+#include <linux/err.h>
+#include <linux/bitmap.h>
 
 #include "format-block.h"
 #include "balloc.h"
 #include "block.h"
 #include "pr.h"
+#include "seqlock.h"
 #include "super.h"
 #include "txn.h"
 
-/*
- * We don't have block allocators yet.  Every mount assumes it's the
- * only mount and is starting with a fresh system.  Allocations advance
- * from the first possible block and frees are ignored.
- */
+#define REGF		"reg bnr %llu nr_set %lu"
+#define REGA(reg)	(reg)->base_bnr, (reg)->nr_set
+
 struct rpdfs_balloc_info {
-	atomic64_t next_bnr;
+	struct rpdfs_balloc_region * __percpu *pcpu_region;
+	struct wait_queue_head waitq;
+	seqlock_t seqlock;
+	struct list_head region_list;
+	atomic_t enospc_count;
 };
 
-static inline struct rpdfs_balloc_info *RPDFS_ALINF(struct rpdfs_fs_info *rfi)
+static inline struct rpdfs_balloc_info *RPDFS_BALINF(struct rpdfs_fs_info *rfi)
 {
 	return rfi->balloc_info;
 }
 
-static inline void SET_RPDFS_ALINF(struct rpdfs_fs_info *rfi, struct rpdfs_balloc_info *alinf)
+static inline void SET_RPDFS_BALINF(struct rpdfs_fs_info *rfi, struct rpdfs_balloc_info *balinf)
 {
-	rfi->balloc_info = alinf;
+	rfi->balloc_info = balinf;
 }
 
-/*
- * When we get allocator blocks this will look ahead in prepared
- * allocator blocks to prepare write references to allocated blocks.
- * Until then we just grab a chunk of the per-mount allocator atomic.
- *
- * It's tricky to use the peeked value.  The caller needs to be sure to
- * perform the apply calls in the same sequence as they consumed the
- * peeked values.  Ideally callers will be few and obvious (only the
- * first peek/apply in the txn).
- */
-#define ALLOCS_PER_TXN 32
-int rpdfs_balloc_prepare_alloc_peek(struct rpdfs_fs_info *rfi, struct rpdfs_transaction *txn,
-				    u64 *bnr_ret)
+struct rpdfs_balloc_region *rpdfs_balloc_alloc_region(u64 base_bnr, unsigned long nr_blocks)
 {
-	struct rpdfs_balloc_info *alinf = RPDFS_ALINF(rfi);
-	struct rpdfs_block_handle *hnd = NULL;
-	u64 bnr;
-	int ret;
+	struct rpdfs_balloc_region *reg;
 
-	if (txn->alloc_base == 0) {
-		txn->alloc_base = atomic64_add_return(ALLOCS_PER_TXN, &alinf->next_bnr) -
-				  ALLOCS_PER_TXN;
-		txn->alloc_nr = ALLOCS_PER_TXN;
-	}
-
-	if (WARN_ON_ONCE(txn->alloc_used == txn->alloc_nr)) {
-		ret = -ENOSPC;
+	reg = kvmalloc(offsetof(struct rpdfs_balloc_region,
+				bits[DIV_ROUND_UP(nr_blocks, BITS_PER_LONG)]), GFP_NOFS);
+	if (!reg) {
+		reg = ERR_PTR(-ENOMEM);
 		goto out;
 	}
 
-	bnr = txn->alloc_base + txn->alloc_used;
-
-	ret = rpdfs_txn_prepare_acquire(rfi, txn, bnr, &hnd);
-	if (ret == 0) {
-		rpdfs_txn_prepare_release(rfi, txn, &hnd, RBAF_WRITE | RBAF_OVERWRITE);
-		txn->alloc_used++;
-		*bnr_ret = bnr;
-	}
-
+	INIT_LIST_HEAD(&reg->head);
+	reg->base_bnr = base_bnr;
+	reg->size = nr_blocks;
+	reg->first_set = reg->size;
+	reg->nr_set = 0;
+	bitmap_zero(reg->bits, reg->size);
+	rpdfs_prd(REGF, REGA(reg));
 out:
-	if (ret < 0)
-		*bnr_ret = 0;
-
-	rpdfs_prd("base %llu used %llu nr %llu bnr %llu ret %d",
-		  txn->alloc_base, txn->alloc_used, txn->alloc_nr, *bnr_ret, ret);
-
-	return ret;
+	return reg;
 }
 
-int rpdfs_balloc_prepare_alloc(struct rpdfs_fs_info *rfi, struct rpdfs_transaction *txn)
+void rpdfs_balloc_free_region(struct rpdfs_balloc_region *reg)
 {
-	u64 ignored;
-	return rpdfs_balloc_prepare_alloc_peek(rfi, txn, &ignored);
+	if (!IS_ERR_OR_NULL(reg)) {
+		rpdfs_prd(REGF, REGA(reg));
+		kvfree(reg);
+	}
 }
 
-void rpdfs_balloc_reset_prepare(struct rpdfs_fs_info *rfi, struct rpdfs_transaction *txn)
+void rpdfs_balloc_set_stripe_bits(struct rpdfs_balloc_region *reg, unsigned long this_stripe,
+				  unsigned long stripes, __le64 *bmap, unsigned long size)
 {
-	txn->alloc_used = 0;
-}
+	unsigned long b;
+	unsigned long r;
 
-void rpdfs_balloc_start_apply(struct rpdfs_fs_info *rfi, struct rpdfs_transaction *txn)
-{
-	txn->alloc_nr = txn->alloc_used;
-	txn->alloc_used = 0;
+	for (b = 0; (b = find_next_bit_le(bmap, size, b)) < size; b++) {
+		r = this_stripe + (b * stripes);
+		set_bit(r, reg->bits);
+		if (r < reg->first_set)
+			reg->first_set = r;
+		reg->nr_set++;
+	}
 }
 
 /*
- * Apply and return the result of an allocation that was prepared by
- * _prepare_alloc.  The caller can use _txn_use_prepared() to get the
- * allocated block.
+ * Return the next free block, starting at the given position.  If we
+ * find one then set the position to one past what we found.
+ *
+ * Returns -ENOSPC if there are none available from the position.
  */
-int rpdfs_balloc_apply_alloc(struct rpdfs_fs_info *rfi, struct rpdfs_transaction *txn,
-			     u64 *bnr_ret)
+int rpdfs_balloc_find_next(struct rpdfs_balloc_region *reg, unsigned long *pos, u64 *bnr_ret)
 {
+	unsigned long b;
 	int ret;
 
-	if (WARN_ON_ONCE(txn->alloc_base == 0 || txn->alloc_used == txn->alloc_nr)) {
-		*bnr_ret = 0;
+	b = find_next_bit(reg->bits, reg->size, max(*pos, reg->first_set));
+	if (b >= reg->size) {
+		*pos = reg->size;
 		ret = -ENOSPC;
 	} else {
-		*bnr_ret = txn->alloc_base + txn->alloc_used++;
+		*pos = b + 1;
+		*bnr_ret = reg->base_bnr + b;
 		ret = 0;
 	}
 
-	rpdfs_prd("base %llu used %llu nr %llu bnr %llu ret %d",
-		  txn->alloc_base, txn->alloc_used, txn->alloc_nr, *bnr_ret, ret);
+	rpdfs_prd(REGF" bnr %llu ret %d after_pos %lu", REGA(reg), *bnr_ret, ret, *pos);
 
 	return ret;
+}
+
+void rpdfs_balloc_clear(struct rpdfs_balloc_region *reg, u64 bnr)
+{
+	int b = bnr - reg->base_bnr;
+
+	if (WARN_ON_ONCE(bnr < reg->base_bnr || bnr >= (reg->base_bnr + reg->size) ||
+			 !test_bit(b, reg->bits)))
+		return;
+
+	rpdfs_prd(REGF" bnr %llu", REGA(reg), bnr);
+
+	clear_bit(b, reg->bits);
+	if (reg->first_set == b)
+		reg->first_set++;
+	reg->nr_set--;
+
+}
+
+/*
+ * The block cache has completed a wave of requests for free stripes in
+ * a region.  We either put a populated region on the list or record the
+ * arrival of an empty region as an instance of enospc to be consumed.
+ */
+void rpdfs_balloc_publish_region(struct rpdfs_fs_info *rfi, struct rpdfs_balloc_region *reg)
+{
+	struct rpdfs_balloc_info *balinf = RPDFS_BALINF(rfi);
+
+	rpdfs_prd(REGF, REGA(reg));
+
+	if (reg->nr_set == 0) {
+		rpdfs_balloc_free_region(reg);
+		atomic_inc(&balinf->enospc_count);
+	} else {
+		write_seqlock(&balinf->seqlock);
+		list_add_tail(&reg->head, &balinf->region_list);
+		write_sequnlock(&balinf->seqlock);
+	}
+	wake_up(&balinf->waitq);
+}
+
+static bool ready_or_resend(struct rpdfs_fs_info *rfi, struct rpdfs_balloc_info *balinf, u64 until)
+{
+	bool empty;
+
+	while_read_seqretry(&balinf->seqlock)
+		empty = list_empty(&balinf->region_list);
+	if (!empty || atomic_read(&balinf->enospc_count) > 0)
+		return true;
+
+	return rpdfs_block_should_request_free(rfi, until);
+}
+
+/*
+ * Get exclusive access to a region of probably free blocks.  Typically
+ * a busy task will be handing a region to and from its per-cpu region.
+ *
+ * When we don't have any prepared we ask the block cache to assemble a
+ * new free region by requesting free stripes from the devds.
+ *
+ * ENOSPC isn't handled particularly gracefully.  Each arrival of an
+ * empty region causes one waiting task, who has already sent their
+ * requests, to return enospc.
+ *
+ * The only attempt at fairness is in having exclusive waits for the
+ * list to be populated.  The longest waiter is woken first.  We could
+ * use wake functions to stop new arrivals from removing from the list
+ * early.
+ */
+struct rpdfs_balloc_region *rpdfs_balloc_take_region(struct rpdfs_fs_info *rfi)
+{
+	struct rpdfs_balloc_info *balinf = RPDFS_BALINF(rfi);
+	struct rpdfs_balloc_region **preg;
+	struct rpdfs_balloc_region *reg;
+	u64 until = 0;
+	bool empty;
+	int ret;
+
+	for (;;) {
+		reg = NULL;
+		preg = get_cpu_ptr(balinf->pcpu_region);
+		if (*preg)
+			swap(reg, *preg);
+		put_cpu_ptr(balinf->pcpu_region);
+		if (reg)
+			break;
+
+		while_read_seqretry(&balinf->seqlock)
+			empty = list_empty(&balinf->region_list);
+		if (!empty) {
+			write_seqlock(&balinf->seqlock);
+			reg = list_first_entry_or_null(&balinf->region_list,
+						       struct rpdfs_balloc_region, head);
+			if (reg)
+				list_del_init(&reg->head);
+			write_sequnlock(&balinf->seqlock);
+		}
+		if (reg)
+			break;
+
+		if (rpdfs_block_should_request_free(rfi, until)) {
+			if (until > 0 && atomic_dec_if_positive(&balinf->enospc_count) >= 0) {
+				reg = ERR_PTR(-ENOSPC);
+				break;
+			}
+
+			ret = rpdfs_block_request_free(rfi, &until);
+			if (ret < 0) {
+				reg = ERR_PTR(ret);
+				break;
+			}
+		}
+
+		ret = wait_event_interruptible_exclusive(balinf->waitq,
+							 ready_or_resend(rfi, balinf, until));
+		if (ret < 0) {
+			reg = ERR_PTR(ret);
+			break;
+		}
+	}
+
+	if (!IS_ERR_OR_NULL(reg))
+		rpdfs_prd(REGF, REGA(reg));
+
+	return reg;
+}
+
+void rpdfs_balloc_return_region(struct rpdfs_fs_info *rfi, struct rpdfs_balloc_region *reg)
+{
+	struct rpdfs_balloc_info *balinf = RPDFS_BALINF(rfi);
+	struct rpdfs_balloc_region **preg;
+
+	if (IS_ERR_OR_NULL(reg))
+		return;
+
+	rpdfs_prd(REGF, REGA(reg));
+
+	if (reg->nr_set == 0) {
+		rpdfs_balloc_free_region(reg);
+		return;
+	}
+
+	preg = get_cpu_ptr(balinf->pcpu_region);
+	if (*preg == NULL) {
+		*preg = reg;
+		reg = NULL;
+	}
+	put_cpu_ptr(balinf->pcpu_region);
+
+	if (reg) {
+		write_seqlock(&balinf->seqlock);
+		list_add(&reg->head, &balinf->region_list);
+		write_sequnlock(&balinf->seqlock);
+		wake_up(&balinf->waitq);
+	}
 }
 
 int rpdfs_balloc_free_meta(struct rpdfs_fs_info *rfi, struct rpdfs_transaction *txn, u64 bnr)
@@ -125,18 +266,24 @@ int rpdfs_balloc_free_meta(struct rpdfs_fs_info *rfi, struct rpdfs_transaction *
 
 int rpdfs_balloc_setup(struct rpdfs_fs_info *rfi)
 {
-	struct rpdfs_balloc_info *alinf;
+	struct rpdfs_balloc_info *balinf;
 	int ret;
 
-	alinf = kzalloc(sizeof(struct rpdfs_balloc_info), GFP_KERNEL);
-	if (!alinf) {
+	balinf = kzalloc(sizeof(struct rpdfs_balloc_info), GFP_KERNEL);
+	if (balinf)
+		balinf->pcpu_region = alloc_percpu(struct rpdfs_balloc_region *);
+	if (!balinf || !balinf->pcpu_region) {
+		kfree(balinf);
 		ret = -ENOMEM;
 		goto out;
 	}
 
-	atomic64_set(&alinf->next_bnr, RPDFS_ROOT_INO + 1);
+	init_waitqueue_head(&balinf->waitq);
+	seqlock_init(&balinf->seqlock);
+	INIT_LIST_HEAD(&balinf->region_list);
+	atomic_set(&balinf->enospc_count, 0);
 
-	SET_RPDFS_ALINF(rfi, alinf);
+	SET_RPDFS_BALINF(rfi, balinf);
 	ret = 0;
 out:
 	return ret;
@@ -144,10 +291,24 @@ out:
 
 void rpdfs_balloc_destroy(struct rpdfs_fs_info *rfi)
 {
-	struct rpdfs_balloc_info *alinf = RPDFS_ALINF(rfi);
+	struct rpdfs_balloc_info *balinf = RPDFS_BALINF(rfi);
+	struct rpdfs_balloc_region *reg;
+	struct rpdfs_balloc_region *_reg_;
+	int cpu;
 
-	if (alinf) {
-		SET_RPDFS_ALINF(rfi, NULL);
-		kfree(alinf);
+	if (balinf) {
+		for_each_possible_cpu(cpu) {
+			reg = *per_cpu_ptr(balinf->pcpu_region, cpu);
+			rpdfs_balloc_free_region(reg);
+		}
+
+		list_for_each_entry_safe(reg, _reg_, &balinf->region_list, head) {
+			list_del_init(&reg->head);
+			rpdfs_balloc_free_region(reg);
+		}
+
+		free_percpu(balinf->pcpu_region);
+		kfree(balinf);
+		SET_RPDFS_BALINF(rfi, NULL);
 	}
 }
--- fs/rpdfs/balloc.h
+++ fs/rpdfs/balloc.h
@@ -2,15 +2,38 @@
 #ifndef RPDFS_BALLOC_H
 #define RPDFS_BALLOC_H
 
+#include <linux/types.h>
+
+#include "format-msg.h"
 #include "txn.h"
 
-int rpdfs_balloc_prepare_alloc_peek(struct rpdfs_fs_info *rfi, struct rpdfs_transaction *txn,
-				    u64 *bnr_ret);
-int rpdfs_balloc_prepare_alloc(struct rpdfs_fs_info *rfi, struct rpdfs_transaction *txn);
-void rpdfs_balloc_reset_prepare(struct rpdfs_fs_info *rfi, struct rpdfs_transaction *txn);
-void rpdfs_balloc_start_apply(struct rpdfs_fs_info *rfi, struct rpdfs_transaction *txn);
-int rpdfs_balloc_apply_alloc(struct rpdfs_fs_info *rfi, struct rpdfs_transaction *txn,
-			     u64 *bnr_ret);
+/*
+ * A record of blocks that were free and granted write mode when the
+ * block cache received free stripe grants.
+ *
+ * By the time we try to allocate them they might have been allocated
+ * elsewhere or revoked.
+ */
+struct rpdfs_balloc_region {
+	struct list_head head;
+	u64 base_bnr;
+	unsigned long size;
+	unsigned long first_set;
+	unsigned long nr_set;
+	unsigned long bits[];
+};
+
+struct rpdfs_balloc_region *rpdfs_balloc_alloc_region(u64 base_bnr, unsigned long nr_blocks);
+void rpdfs_balloc_free_region(struct rpdfs_balloc_region *reg);
+void rpdfs_balloc_set_stripe_bits(struct rpdfs_balloc_region *reg, unsigned long this_stripe,
+				  unsigned long stripes, __le64 *bmap, unsigned long size);
+void rpdfs_balloc_publish_region(struct rpdfs_fs_info *rfi, struct rpdfs_balloc_region *reg);
+
+int rpdfs_balloc_find_next(struct rpdfs_balloc_region *reg, unsigned long *pos, u64 *bnr_ret);
+void rpdfs_balloc_clear(struct rpdfs_balloc_region *reg, u64 bnr);
+
+struct rpdfs_balloc_region *rpdfs_balloc_take_region(struct rpdfs_fs_info *rfi);
+void rpdfs_balloc_return_region(struct rpdfs_fs_info *rfi, struct rpdfs_balloc_region *reg);
 
 int rpdfs_balloc_free_meta(struct rpdfs_fs_info *rfi, struct rpdfs_transaction *txn, u64 bnr);
 
