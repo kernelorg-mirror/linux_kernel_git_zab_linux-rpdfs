c777faff54495135191a2fa5d2de1b8ef0ce6054
Zach Brown <zab@zabbo.net>
2026-03-24 12:45:08 -0700

    rpdfs: update btree io/txn layer
    Update the btree_txn layer to work with block handles instead of the
    prepare/apply model of atomic changes.  Callers now make their way
    through transactions one change at a time and we have very simple btree
    traversals that split/merge on descent and modify leaves.  Callers undo
    changes if they see errors.
    
    We get rid of all the prepare/apply shenanigans and end up with a
    handful of operation wrappers that each call traversal and then their
    respective core function to operate on items in leaf blocks.
    
    Typically we only have a handle on one block at a time.  Callers provide
    locking by holding read or write handles to the root of the btree while
    we're called.  When split and merge have to hold multiple handles they
    are careful to only block while acquiring in order: parents to children
    and left to right.
    
    Signed-off-by: Zach Brown <zab@zabbo.net>

--- fs/rpdfs/btree_txn.h
+++ fs/rpdfs/btree_txn.h
@@ -2,32 +2,22 @@
 #ifndef RPDFS_BTREE_TXN_H
 #define RPDFS_BTREE_TXN_H
 
+#include "format-block.h"
 #include "btree.h"
 #include "txn.h"
 
-int rpdfs_btree_txn_prepare_insert(struct rpdfs_fs_info *rfi, struct rpdfs_transaction *txn,
-				   struct rpdfs_btree_root *root, struct rpdfs_btree_key *key,
-				   unsigned val_size, rpdfs_btree_item_cb_t item_cb, void *cb_arg);
-int rpdfs_btree_txn_apply_insert(struct rpdfs_fs_info *rfi, struct rpdfs_transaction *txn,
-				 struct rpdfs_btree_root *root, struct rpdfs_btree_key *key,
-				 unsigned val_size, rpdfs_btree_item_cb_t item_cb, void *cb_arg);
-int rpdfs_btree_txn_prepare_delete(struct rpdfs_fs_info *rfi, struct rpdfs_transaction *txn,
-				   struct rpdfs_btree_root *root, struct rpdfs_btree_key *key,
-				   unsigned val_size, rpdfs_btree_item_cb_t item_cb, void *cb_arg);
-int rpdfs_btree_txn_apply_delete(struct rpdfs_fs_info *rfi, struct rpdfs_transaction *txn,
-				 struct rpdfs_btree_root *root, struct rpdfs_btree_key *key,
-				 unsigned val_size, rpdfs_btree_item_cb_t item_cb, void *cb_arg);
-int rpdfs_btree_txn_prepare_modify(struct rpdfs_fs_info *rfi, struct rpdfs_transaction *txn,
-				   struct rpdfs_btree_root *root, struct rpdfs_btree_key *key,
-				   rpdfs_btree_item_cb_t item_cb, void *cb_arg);
-int rpdfs_btree_txn_apply_modify(struct rpdfs_fs_info *rfi, struct rpdfs_transaction *txn,
-				 struct rpdfs_btree_root *root, struct rpdfs_btree_key *key,
-				 rpdfs_btree_item_cb_t item_cb, void *cb_arg);
-int rpdfs_btree_txn_prepare_lookup(struct rpdfs_fs_info *rfi, struct rpdfs_transaction *txn,
-				   struct rpdfs_btree_root *root, struct rpdfs_btree_key *key,
-				   rpdfs_btree_item_cb_t item_cb, void *cb_arg);
-int rpdfs_btree_txn_prepare_copy_items(struct rpdfs_fs_info *rfi, struct rpdfs_transaction *txn,
-				       struct rpdfs_btree_root *root, struct rpdfs_btree_key *key,
-				       void *buf, size_t size);
+int rpdfs_btree_lookup(struct rpdfs_fs_info *rfi, struct rpdfs_btree_root *root,
+		       u64 key, rpdfs_btree_item_cb_t cb, void *arg);
+int rpdfs_btree_insert(struct rpdfs_fs_info *rfi, struct rpdfs_transaction *txn,
+		       struct rpdfs_btree_root *root, u64 key, rpdfs_btree_item_cb_t cb, void *arg,
+		       struct kvec *kv, unsigned nr_segs);
+int rpdfs_btree_modify(struct rpdfs_fs_info *rfi, struct rpdfs_transaction *txn,
+		       struct rpdfs_btree_root *root, u64 key, rpdfs_btree_item_cb_t cb,
+		       void *arg);
+int rpdfs_btree_delete(struct rpdfs_fs_info *rfi, struct rpdfs_transaction *txn,
+		       struct rpdfs_btree_root *root, u64 key, rpdfs_btree_item_cb_t cb,
+		       void *arg);
+int rpdfs_btree_read_items(struct rpdfs_fs_info *rfi, struct rpdfs_btree_root *root,
+			   u64 key, rpdfs_btree_item_cb_t cb, void *arg);
 
 #endif
