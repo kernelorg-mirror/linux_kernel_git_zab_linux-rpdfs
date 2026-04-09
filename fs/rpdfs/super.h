b34839035b203c923f5f1c0769a381f4666629af
Zach Brown <zab@zabbo.net>
2026-04-09 09:19:22 -0700

    rpdfs: add RPDFS_DENTRY_FS
    Add another helper for getting the rfi, this time from dentries.
    
    Signed-off-by: Zach Brown <zab@zabbo.net>

--- fs/rpdfs/super.h
+++ fs/rpdfs/super.h
@@ -21,6 +21,8 @@ struct rpdfs_fs_info {
 	struct rpdfs_block_info *block_info;
 	struct rpdfs_map_info *map_info;
 	struct rpdfs_net_info *net_info;
+
+	struct percpu_counter pc_creates;
 };
 
 static inline struct rpdfs_fs_info *RPDFS_SB_FS(struct super_block *sb)
@@ -33,4 +35,8 @@ static inline struct rpdfs_fs_info *RPDFS_INODE_FS(struct inode *inode)
 	return RPDFS_SB_FS(inode->i_sb);
 }
 
+static inline struct rpdfs_fs_info *RPDFS_DENTRY_FS(struct dentry *dentry)
+{
+	return RPDFS_INODE_FS(d_inode(dentry));
+}
 #endif
