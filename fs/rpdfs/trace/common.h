7dd9571ed4664ad741e8519b4a4369a2d84523f6
Zach Brown <zab@zabbo.net>
2026-07-30 14:45:22 -0700

    rpdfs: terminate fs_info trace assignment
    In this sea of CPP nonsense it seems more consistent to have the
    assignments in these type macros terminated.  It'd be weird if the user
    terminated and all but the last in the definition were terminated?  Meh.
    
    Signed-off-by: Zach Brown <zab@zabbo.net>

--- fs/rpdfs/trace/common.h
+++ fs/rpdfs/trace/common.h
@@ -6,6 +6,6 @@
 	__typeof__(rfi) _rfi = (rfi); \
 	(_rfi) ? le32_to_cpup((__le32 *)(void *)_rfi->client_uuid) >> 8 : 0; \
 })
-#define RFI_TRACE_ASSIGN(rfi)	__entry->id = RFI_TRACE_ID(rfi)
+#define RFI_TRACE_ASSIGN(rfi)	__entry->id = RFI_TRACE_ID(rfi); /* intentional semicolon */
 #define RFI_TRACE_TPF		"%06x"
 #define RFI_TRACE_TPA		__entry->id
